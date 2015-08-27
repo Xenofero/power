f658078e61788941cef256dc5eeff3c5ae0287f3
xeno <xeno@energy-server>
2015-08-28 01:31:05 +0200

    Input klappt, speichern buggy, ausgabe fehlt

--- flaskapp/app/forms.py
+++ flaskapp/app/forms.py
@@ -1,8 +1,8 @@
-from flask.ext.wtf import Form
-from wtforms.fields import TextField, DecimalField, SubmitField
- 
+from flask_wtf import Form
+from wtforms import TextField, DecimalField, SubmitField, validators
+
 class AddForm(Form):
-  device = TextField("device")
-  energyusage = DecimalField("energyusage(W)")
-  hoursperweek = DecimalField("houerperweek")
+  device = TextField("device",  [validators.DataRequired("What device do you wish to add?")])
+  energyusage = DecimalField("energyusage(W)",  [validators.DataRequired("How much energy need the device?"), validators.NumberRange(min=0, max=5000, message= "have to be between 0 and 5000")])
+  hoursperweek = DecimalField("houerperweek",  [validators.DataRequired("How many hours per week is this device on?"), validators.NumberRange(min=0, max=168, message= "have to be between 0 and 168")])
   submit = SubmitField("save")
--- flaskapp/app/routes.py
+++ flaskapp/app/routes.py
@@ -1,21 +1,41 @@
-from flask import Flask, render_template, request
+# -*- coding: iso-8859-1 -*-
+from flask import Flask, render_template, request, flash
 from forms import AddForm
- 
-app = Flask(__name__)      
-#app.secret_key = 'energy key'
- 
+import shelve
+
+app = Flask(__name__)
+app.secret_key = 'energy key'
+
 @app.route('/')
 def home():
-  return render_template('home.html')
+    return render_template('home.html')
+
 
 @app.route('/adddevice', methods=['GET', 'POST'])
 def adddevice():
-  if request.method == 'POST':
-    return 'device added'
-
-  elif request.method == 'POST':
     form = AddForm()
-    return render_template('add.html', form=form)
- 
+
+    # Eingabe
+    if request.method == 'POST':
+        # Fehler
+        if form.validate() == False:
+            flash('Error, validation faild.')
+            return render_template('add.html', form=form)
+
+        # Rückgabe + Speichern
+        else:
+            device = eval(form.device.data)
+            energy = eval(form.energyusage.data)
+            hours = eval(form.hoursperweek.data)
+            sh = shelve.open("devices.slv", flag="c")
+            sh[device] = {'energy': energy, 'hours': hours}
+            sh.close()
+
+            return render_template('add.html', success=True, device=device)
+
+    elif request.method == 'GET':
+        return render_template('add.html', form=form)
+
+
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
